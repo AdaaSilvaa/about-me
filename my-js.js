08de7482342553a87a771aaae0ac6d7f7cd284f3
AdaaSilvaa <41088890+AdaaSilvaa@users.noreply.github.com>
2018-07-10 15:16:03 -0700

    Create my-js.js

---
+++ my-js.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",
+function() {
+    alert("Hello World!");
+    console.log("JEUUUU");
+
+    //let myPhoto = document.getElementById("personal-image")
+    //myPhoto.addEventListener("click", changeImage)
+
+
+//function changeImage(){
+    //console.log(myPhoto.rsc);
+
+}
+    //all of your cocde goes here
+
+}) // END OF DOM CONTENT LOADED
